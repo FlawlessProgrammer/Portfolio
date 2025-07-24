cabbfe37d8f7496a14157945bc508e6d5c52adbd
Flawless Coder <157590472+FlawlessProgrammer@users.noreply.github.com>
2025-07-24 13:32:07 +0100

    July 24

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,25 +1,31 @@
-let menuIcon = document.querySelector('#menu-icon');
+let hamburger = document.querySelector('.hamburger');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('.section');
-let navlinks = querySelectorAll('.header .nav a');
+let icon = hamburger.querySelector('i');
+// let sections = document.querySelector('section');
+// let navlinks = querySelector('.header nav a');
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links => {
-                navlinks.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
-            })
-        }
-    })
-}
+//         if(top >= offset && top < offset + height){
+//             navlinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+//             });
+//         }
+//     });
+// }
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-window-close');
+hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active');
     navbar.classList.toggle('active');
-}
+
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-times');
+    console.log('clicked');
+    
+})
